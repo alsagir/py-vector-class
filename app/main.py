530ba5240fb48ec9ea051416a0217dce40db752d
alsagir <alsagir387@gmail.com>
2024-07-25 23:36:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ class Vector:
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            self.x + other.x,
+            self.y + other.y
         )
 
     def __sub__(self, other: "Vector") -> "Vector":
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            self.x - other.x,
+            self.y - other.y
         )
 
     def __mul__(self,
